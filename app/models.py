5e9cae1a828af9b8e4fa5d33d6aefef00d9c6b1f
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-11-16 10:01:49 +0300

    Add user class.

---
+++ app/models.py
@@ -0,0 +1,39 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(200),index = True)
+    email = db.Column(db.String(200),unique = True,index = True)
+    about = db.Column(db.String(200))
+    profile_pic_path = db.Column(db.String)
+    password_hash = db.Column(db.String(200))
+    journals = db.relationship('Journal',backref='user',lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+# class Journal(db.Model):
