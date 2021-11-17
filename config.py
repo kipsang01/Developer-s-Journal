3d0c32c024fc1ae3b157bc16d84caba9a30f37ff
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-17 15:21:41 +0300

    Changed database configurations

--- config.py
+++ config.py
@@ -25,10 +25,10 @@ class ProdConfig(Config):
 
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ann:Ann123@localhost/journal'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://annalis:Ann123@localhost/journal'
     
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ann:Ann123@localhost/journal'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://annalis:Ann123@localhost/journal'
 
 
     DEBUG = True
